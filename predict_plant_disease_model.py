e1b7ffd79334b7c1d64dd2212fe4f5efda257e60
SARVESH CHAUDHARY <141572008+sarveshrocks8@users.noreply.github.com>
2025-11-14 15:00:21 +0530

    Increase image size for model prediction

--- predict_plant_disease_model.py
+++ predict_plant_disease_model.py
@@ -31,7 +31,7 @@ class_labels = {
 
 # ✅ Load and preprocess image
 img_path = r"C:\Users\Dell\OneDrive\Pictures\Screenshots\potato early.jpg"
-img = image.load_img(img_path, target_size=(64, 64))
+img = image.load_img(img_path, target_size=(128, 128))
 img_array = image.img_to_array(img) / 255.0
 img_array = np.expand_dims(img_array, axis=0)
 
